47ffb8927fe51cede66fcd43f077a7c30e3201e5
brotzky <brotzky@gmail.com>
2019-05-16 15:37:33 -0700

    Removing hash

--- gatsby/onCreateWebpackConfig.js
+++ gatsby/onCreateWebpackConfig.js
@@ -2,7 +2,7 @@
 
 const path = require('path')
 
-module.exports = ({ actions }) => {
+module.exports = ({ actions, getConfig, stage }) => {
   actions.setWebpackConfig({
     resolve: {
       alias: {
@@ -14,4 +14,29 @@ module.exports = ({ actions }) => {
       extensions: ['.js', '.json', '.ts', '.tsx'],
     },
   })
+
+  /**
+   * Removing cache-busting filenames to avoid Missing Resources /.
+   * This is tailored specifically to Netlify as their deployment process suggests
+   * avoiding all cache-busting filenames.
+   *
+   * Gatsby issue covering the topic with replies from Netlify staff
+   * https://github.com/gatsbyjs/gatsby/issues/11961
+   *
+   * Netlify explanation about their caching process
+   * https://www.netlify.com/blog/2017/02/23/better-living-through-caching/
+   */
+  if (stage === 'build-javascript') {
+    const newWebpackConfig = {
+      ...getConfig(),
+      output: {
+        filename: `[name].js`,
+        chunkFilename: `[name].js`,
+        path: getConfig().output.path,
+        publicPath: getConfig().output.publicPath,
+      },
+    }
+
+    actions.replaceWebpackConfig(newWebpackConfig)
+  }
 }
