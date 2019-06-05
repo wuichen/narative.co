91f0556f54da475c94d41d03d3d7bab34871edd5
brotzky <brotzky@gmail.com>
2019-06-04 19:43:05 -0700

    Moving careers below labs

--- src/shortcuts/shortcuts.tsx
+++ src/shortcuts/shortcuts.tsx
@@ -42,16 +42,16 @@ const shortcuts = [
     icon: icons.OpenSourceIcon,
   },
   {
-    name: constants.GO_TO_CAREERS,
-    keys: ['G', 'C'],
-    label: ['Go to ', 'Careers'],
-    icon: icons.LightbulbIcon,
-  },
-  {
     name: constants.GO_TO_LABS,
     keys: ['G', 'L'],
     label: ['Go to', ' Labs'],
     icon: icons.LaptopIcon,
+  },
+  {
+    name: constants.GO_TO_CAREERS,
+    keys: ['G', 'C'],
+    label: ['Go to ', 'Careers'],
+    icon: icons.LightbulbIcon,
   },
   {
     name: constants.GO_TO_HOME,
