d7342b8a76d7fc9f725d95c6da67e4d9ebfb82d9
brotzky <brotzky@gmail.com>
2019-07-09 16:39:45 -0700

    removing netlify cache busting

--- gatsby/onCreateWebpackConfig.js
+++ gatsby/onCreateWebpackConfig.js
@@ -16,28 +16,4 @@ module.exports = ({ actions, getConfig, stage }) => {
       extensions: ['.js', '.json', '.ts', '.tsx'],
     },
   })
-
-  /**
-   * This is tailored specifically to Netlify as their deployment process suggests
-   * avoiding all cache-busting filenames.
-   *
-   * Gatsby issue covering the topic with replies from Netlify staff
-   * https://github.com/gatsbyjs/gatsby/issues/11961
-   *
-   * Netlify explanation about their caching process
-   * https://www.netlify.com/blog/2017/02/23/better-living-through-caching/
-   */
-  if (stage === 'build-javascript') {
-    const newWebpackConfig = {
-      ...getConfig(),
-      output: {
-        filename: `[name].js`,
-        chunkFilename: `[name].js`,
-        path: getConfig().output.path,
-        publicPath: getConfig().output.publicPath,
-      },
-    }
-
-    actions.replaceWebpackConfig(newWebpackConfig)
-  }
 }
