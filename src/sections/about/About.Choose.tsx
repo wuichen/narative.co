1d2d2124bac6accd9432562ada8b6e2d5ca34939
brotzky <brotzky@gmail.com>
2019-10-10 15:52:53 -0700

    Choose section

--- src/sections/about/About.Choose.tsx
+++ src/sections/about/About.Choose.tsx
@@ -1,86 +1,155 @@
 import React, { useContext } from 'react'
 import styled from 'styled-components'
+import { graphql, useStaticQuery } from 'gatsby'
+import SVG from 'react-inlinesvg'
 
 import ButtonPill from '@components/Button/Button.Pill'
-import Divider from '@components/Divider'
 import Heading from '@components/Heading'
 import Section from '@components/Section'
+import Sticky from '@components/Sticky'
+import { ContactContext } from '@components/Contact/Contact.Context'
+
 import mediaqueries from '@styles/media'
 
 import AboutHeading from './About.Heading'
 
-import { ContactContext } from '@components/Contact/Contact.Context'
+const shapeIlloQuery = graphql`
+  query GetShapeIllo {
+    shape: file(name: { regex: "/about-glow-shape/" }) {
+      publicURL
+    }
+  }
+`
 
 function AboutChoose() {
+  const { shape } = useStaticQuery(shapeIlloQuery)
   const { toggleContact } = useContext(ContactContext)
 
+  console.log(shape)
   return (
     <AboutChooseContainer>
-      <AboutHeading
-        heading="Who we choose to be"
-        text="A company's culture isn’t something to be passed down as commandments, or enforced like law. It's the choices we make every day that defines who we are — as individuals, and as a team. These are our choices."
+      <Sticky
+        height="1600px"
+        cover
+        render={({ progress }) =>
+          console.log(progress) || (
+            <AboutChooseInner>
+              <>
+                <div>
+                  <div
+                    style={{
+                      opacity: 1 - (progress + progress + progress),
+                      filter: `blur(${progress * 40}px)`,
+                    }}
+                  >
+                    <AboutHeading
+                      heading="Who we choose to be"
+                      text="A company's culture isn’t something to be passed down as commandments, or enforced like law. It's the choices we make every day that defines who we are — as individuals, and as a team. These are our choices."
+                    />
+                  </div>
+
+                  <Section narrow>
+                    <ShapeContainer
+                      style={{
+                        transform: `scale(${1 + progress * 10})`,
+                      }}
+                    >
+                      <SVG src={shape.publicURL} />
+                    </ShapeContainer>
+                    <Values
+                      style={{
+                        opacity: 1.4 - (1.4 - progress),
+                        filter: `blur(${(0.5 - progress) * 10}px)`,
+                      }}
+                    >
+                      <ValuesGrid>
+                        <div>
+                          <ValueIllo />
+                          <ValueHeading>Be kind</ValueHeading>
+                          <ValueText>
+                            Communicate honestly but sensitively; act with
+                            positive intent; and trust others to do the same.
+                          </ValueText>
+                        </div>
+                        <div>
+                          <ValueIllo />
+                          <ValueHeading>Be creative</ValueHeading>
+                          <ValueText>
+                            Push beyond best practices and by-the-numbers design
+                            to discover valuable new ideas.
+                          </ValueText>
+                        </div>
+                        <div>
+                          <ValueIllo />
+                          <ValueHeading>Be adaptable</ValueHeading>
+                          <ValueText>
+                            Strive to grow from every challenge and change, even
+                            when it means changing your mind.
+                          </ValueText>
+                        </div>
+                        <div>
+                          <ValueIllo />
+                          <ValueHeading>Be yourself</ValueHeading>
+                          <ValueText>
+                            Work and live the way that uniquely suits you, free
+                            of rigid hierarchies and arbitrary expectations.
+                          </ValueText>
+                        </div>
+                      </ValuesGrid>
+                      <ButtonContainer>
+                        <ButtonPill
+                          text="Work with our team"
+                          onClick={toggleContact}
+                        />
+                      </ButtonContainer>
+                    </Values>
+                  </Section>
+                </div>
+              </>
+            </AboutChooseInner>
+          )
+        }
       />
-      <Section narrow>
-        <ShapePlaceholder />
-        <ValuesGrid>
-          <div>
-            <ValueIllo />
-            <ValueHeading>Be kind</ValueHeading>
-            <ValueText>
-              Communicate honestly but sensitively; act with positive intent;
-              and trust others to do the same.
-            </ValueText>
-          </div>
-          <div>
-            <ValueIllo />
-            <ValueHeading>Be creative</ValueHeading>
-            <ValueText>
-              Push beyond best practices and by-the-numbers design to discover
-              valuable new ideas.
-            </ValueText>
-          </div>
-          <div>
-            <ValueIllo />
-            <ValueHeading>Be adaptable</ValueHeading>
-            <ValueText>
-              Strive to grow from every challenge and change, even when it means
-              changing your mind.
-            </ValueText>
-          </div>
-          <div>
-            <ValueIllo />
-            <ValueHeading>Be yourself</ValueHeading>
-            <ValueText>
-              Work and live the way that uniquely suits you, free of rigid
-              hierarchies and arbitrary expectations.
-            </ValueText>
-          </div>
-        </ValuesGrid>
-        <ButtonContainer>
-          <ButtonPill text="Work with our team" onClick={toggleContact} />
-        </ButtonContainer>
-      </Section>
     </AboutChooseContainer>
   )
 }
 
 export default AboutChoose
 
-const ShapePlaceholder = styled.div`
-  margin: 150px auto -100px;
-  width: 100%;
-  height: 580px;
-  background: linear-gradient(rgba(255, 255, 255, 0.5), transparent);
+const ShapeContainer = styled.div`
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  will-change: transform;
+  pointer-events: none;
+  left: 0;
+  right: 0;
+  margin: 0 auto;
 `
 
 const AboutChooseContainer = styled.div`
-  padding: 185px 0 240px;
+  padding: 85px 0 240px;
+`
+
+const AboutChooseInner = styled.div`
+  position: relative;
+  padding: 65px 0 70vh;
+`
+
+const Values = styled.div`
+  position: absolute;
+  width: 100%;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  will-change: transform, filter;
 `
 
 const ValuesGrid = styled.div`
+  top: 0;
   max-width: 750px;
   margin: 0 auto;
-  position: relative;
   display: grid;
   grid-template-columns: 337px 337px;
   grid-template-rows: 1fr 1fr;
