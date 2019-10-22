8b246ad532ea0a7deb2e99281688af8e1cc46531
brotzky <brotzky@gmail.com>
2019-10-22 15:29:53 -0700

    Adjusting photography

--- src/sections/about/About.Photographs.tsx
+++ src/sections/about/About.Photographs.tsx
@@ -98,6 +98,11 @@ function AboutPhotographs() {
   })
   const containerRef = useRef()
   const imageGridRef = useRef()
+  const rowOneRef = useRef()
+  const rowTwoRef = useRef()
+  const rowThreeRef = useRef()
+  const rowFourRef = useRef()
+
   const [ref, inView] = useInView({ threshold: 0.4 })
   const [sectionHeight, setSectionHeight] = useState(2000)
   const [scale, setScale] = useState(1)
@@ -112,9 +117,24 @@ function AboutPhotographs() {
     }
   }, [width])
 
-  const config = { mass: 1, tension: 440, friction: 50 }
+  const config = { mass: 4, tension: 500, friction: 100 }
 
-  const [props, set] = useSpring(() => ({
+  const [propsRowOne, setRowOne] = useSpring(() => ({
+    offset: 1,
+    config,
+  }))
+
+  const [propsRowTwo, setRowTwo] = useSpring(() => ({
+    offset: 1,
+    config,
+  }))
+
+  const [propsRowThree, setRowThree] = useSpring(() => ({
+    offset: 1,
+    config,
+  }))
+
+  const [propsRowFour, setRowFour] = useSpring(() => ({
     offset: 1,
     config,
   }))
@@ -122,9 +142,26 @@ function AboutPhotographs() {
   useEffect(() => {
     const handleScroll = throttle(() => {
       if (imageGridRef.current) {
-        const top = imageGridRef.current.getBoundingClientRect().top
-        const offset = clamp(top / height, 0, 1)
-        set({ offset })
+        const getOffset = top => clamp((top * 2) / height, 0, 1)
+
+        setRowTwo({
+          offset: getOffset(
+            rowOneRef.current.getBoundingClientRect().top +
+              rowOneRef.current.getBoundingClientRect().height / 2.5
+          ),
+        })
+        setRowThree({
+          offset: getOffset(
+            rowTwoRef.current.getBoundingClientRect().top +
+              rowTwoRef.current.getBoundingClientRect().height / 2.5
+          ),
+        })
+        setRowFour({
+          offset: getOffset(
+            rowThreeRef.current.getBoundingClientRect().top +
+              rowThreeRef.current.getBoundingClientRect().height / 2.5
+          ),
+        })
       }
     })
 
@@ -133,10 +170,19 @@ function AboutPhotographs() {
   }, [imageGridRef])
 
   const calcTransform = (offset: number): string =>
-    `translateY(${offset * 25}vh)`
+    `translateY(${offset * 22}vh)`
 
-  const animatedStyles = {
-    transform: props.offset.interpolate(calcTransform),
+  const rowOneStyles = {
+    transform: propsRowOne.offset.interpolate(calcTransform),
+  }
+  const rowTwoStyles = {
+    transform: propsRowTwo.offset.interpolate(calcTransform),
+  }
+  const rowThreeStyles = {
+    transform: propsRowThree.offset.interpolate(calcTransform),
+  }
+  const rowFourStyles = {
+    transform: propsRowFour.offset.interpolate(calcTransform),
   }
 
   return (
@@ -149,10 +195,13 @@ function AboutPhotographs() {
             height={`${sectionHeight}px`}
             render={({ progress }: StickyState) => {
               const scaleFirstGrid = clamp(
-                scale + progress * 3 * (1 - scale),
+                scale + progress * 6 * (1 - scale),
                 0,
                 1
               )
+
+              const headingClass = inView ? 'Heading__InView' : ''
+              const textClass = inView ? 'Text__InView' : ''
 
               return (
                 <div
@@ -165,17 +214,12 @@ function AboutPhotographs() {
                   }}
                   ref={containerRef}
                 >
-                  <div ref={ref}>
+                  <LightSection ref={ref}>
                     <AboutHeading
-                      heading={`<div class="${
-                        inView ? 'Heading__InView' : ''
-                      }">Going the distance</div>`}
-                      text={`<span class="${
-                        inView ? 'Text__InView' : ''
-                      }">Narative is an all-remote team, meaning we work physically apart — something that's taught us the importance of always staying in sync. We communicate constantly and transparently, with each other and with each of our partners.
-    And when it matters most, we make sure to all get together.</span>`}
+                      heading={`<div class="${headingClass}">Going the distance</div>`}
+                      text={`<span class="${textClass}">Narative is an all-remote team, meaning we work physically apart — something that's taught us the importance of always staying in sync. We communicate constantly and transparently, with each other and with each of our partners. And when it matters most, we make sure to all get together.</span>`}
                     />
-
+                    <Spacer />
                     <ImageGrid
                       style={{
                         transform: `scale(${scaleFirstGrid})`,
@@ -183,49 +227,69 @@ function AboutPhotographs() {
                         willChange: 'transform',
                       }}
                     >
-                      <Images>
-                        <ImageLight>
+                      <Images ref={rowOneRef}>
+                        <ImageWrapper>
                           <StyledImage src={lightOne.childImageSharp.fluid} />
-                        </ImageLight>
-                        <ImageLight>
+                        </ImageWrapper>
+                        <ImageWrapper hideOnMobile>
                           <StyledImage src={lightTwo.childImageSharp.fluid} />
-                        </ImageLight>
+                        </ImageWrapper>
                       </Images>
-                      <ImagesReverse>
-                        <ImageLight>
+                      <ImagesMobile>
+                        <ImageWrapper>
+                          <StyledImage src={lightTwo.childImageSharp.fluid} />
+                        </ImageWrapper>
+                        <ImageWrapper>
                           <StyledImage src={lightThree.childImageSharp.fluid} />
-                        </ImageLight>
-                        <ImageLight>
-                          <StyledImage src={lightFour.childImageSharp.fluid} />
-                        </ImageLight>
-                      </ImagesReverse>
+                        </ImageWrapper>
+                      </ImagesMobile>
+
+                      <animated.div style={rowTwoStyles}>
+                        <ImagesReverse ref={rowTwoRef}>
+                          <ImageWrapper hideOnMobile>
+                            <StyledImage
+                              src={lightThree.childImageSharp.fluid}
+                            />
+                          </ImageWrapper>
+                          <ImageWrapper>
+                            <StyledImage
+                              src={lightFour.childImageSharp.fluid}
+                            />
+                          </ImageWrapper>
+                        </ImagesReverse>
+                      </animated.div>
                     </ImageGrid>
-                  </div>
-                  <animated.div style={animatedStyles}>
-                    <ImageGrid
-                      style={{
-                        willChange: 'transform',
-                      }}
-                      ref={imageGridRef}
-                    >
-                      <Images>
-                        <ImageDark>
+                  </LightSection>
+                  <ImageGrid ref={imageGridRef}>
+                    <animated.div style={rowThreeStyles}>
+                      <Images ref={rowThreeRef}>
+                        <ImageWrapper>
                           <StyledImage src={darkOne.childImageSharp.fluid} />
-                        </ImageDark>
-                        <ImageDark>
+                        </ImageWrapper>
+                        <ImageWrapper hideOnMobile>
                           <StyledImage src={darkTwo.childImageSharp.fluid} />
-                        </ImageDark>
+                        </ImageWrapper>
                       </Images>
-                      <ImagesReverse>
-                        <ImageDark>
+                    </animated.div>
+                    <ImagesMobile>
+                      <ImageWrapper>
+                        <StyledImage src={darkTwo.childImageSharp.fluid} />
+                      </ImageWrapper>
+                      <ImageWrapper>
+                        <StyledImage src={darkThree.childImageSharp.fluid} />
+                      </ImageWrapper>
+                    </ImagesMobile>
+                    <animated.div style={rowFourStyles}>
+                      <ImagesReverse ref={rowFourRef}>
+                        <ImageWrapper hideOnMobile>
                           <StyledImage src={darkThree.childImageSharp.fluid} />
-                        </ImageDark>
-                        <ImageDark>
+                        </ImageWrapper>
+                        <ImageWrapper>
                           <StyledImage src={darkFour.childImageSharp.fluid} />
-                        </ImageDark>
+                        </ImageWrapper>
                       </ImagesReverse>
-                    </ImageGrid>
-                  </animated.div>
+                    </animated.div>
+                  </ImageGrid>
                 </div>
               )
             }}
@@ -237,6 +301,12 @@ function AboutPhotographs() {
 }
 
 export default AboutPhotographs
+
+const LightSection = styled.div`
+  position: relative;
+  margin-bottom: -60vh;
+  padding-bottom: 60vh;
+`
 
 const StyledImage = styled(Image)`
   position: absolute;
@@ -251,6 +321,10 @@ const AboutWorkContainer = styled.div`
 
   ${media.tablet`
     padding: 100px 0 calc(180px - 10vh);
+  `}
+
+  ${media.phablet`
+  padding: 90px 0 calc(80px - 10vh);
   `}
 
   .Heading__InView {
@@ -276,12 +350,13 @@ const LightOverlay = styled.div<{ inView: boolean }>`
 
 const Spacer = styled.div`
   height: 10vh;
-  max-height: 100px;
+  max-height: 30px;
 `
 
 const ImageGrid = styled.div`
   position: relative;
   margin: 15px 0;
+  will-change: transform;
 `
 
 const Images = styled.div`
@@ -290,6 +365,23 @@ const Images = styled.div`
   grid-template-rows: 1fr;
   grid-gap: 15px;
   margin-bottom: 15px;
+
+  ${media.tablet`
+    grid-template-columns: 1fr;
+    grid-gap: 10px;
+    margin-bottom: 10px;
+  `}
+`
+
+const ImagesMobile = styled.div`
+  display: none;
+
+  ${media.tablet`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 10px;
+    margin-bottom: 10px;
+  `}
 `
 
 const ImagesReverse = styled.div`
@@ -297,11 +389,17 @@ const ImagesReverse = styled.div`
   grid-template-columns: 33.5fr 66.5fr;
   grid-template-rows: 1fr;
   grid-gap: 15px;
+
+  ${media.tablet`
+    grid-template-columns: 1fr;
+    grid-gap: 10px;
+    margin-bottom: 10px;
+  `}
 `
 
-const ImageLight = styled.div`
-  background: #e1dce1;
+const ImageWrapper = styled.div<{ hideOnMobile?: boolean }>`
   height: 73.4vh;
+  min-height: 320px;
   max-height: 660px;
 
   ${media.desktop`
@@ -310,19 +408,9 @@ const ImageLight = styled.div`
 
   ${media.tablet`
     height: 33vh;
-  `}
-`
+    min-height: 260px;
 
-const ImageDark = styled.div`
-  background: ${p => p.theme.colors.kepler};
-  height: 73.4vh;
-  max-height: 660px;
 
-  ${media.desktop`
-    height: 60vh;
-  `}
-
-  ${media.tablet`
-    height: 33vh;
+    ${p => p.hideOnMobile && `display: none;`}
   `}
 `
