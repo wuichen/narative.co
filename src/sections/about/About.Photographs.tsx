ee42d9639314775091c36c8530a99120bb3dac84
brotzky <brotzky@gmail.com>
2019-11-25 17:02:14 -0800

    removing eager loading

--- src/sections/about/About.Photographs.tsx
+++ src/sections/about/About.Photographs.tsx
@@ -186,30 +186,18 @@ function AboutPhotographs() {
                     <div ref={ref}>
                       <Images ref={rowOneRef}>
                         <ImageWrapper>
-                          <StyledImage
-                            loading="eager"
-                            src={lightOne.childImageSharp.fluid}
-                          />
+                          <StyledImage src={lightOne.childImageSharp.fluid} />
                         </ImageWrapper>
                         <ImageWrapper hideOnMobile>
-                          <StyledImage
-                            loading="eager"
-                            src={lightTwo.childImageSharp.fluid}
-                          />
+                          <StyledImage src={lightTwo.childImageSharp.fluid} />
                         </ImageWrapper>
                       </Images>
                       <ImagesMobile style={rowTwoStyles}>
                         <ImageWrapper>
-                          <StyledImage
-                            loading="eager"
-                            src={lightTwo.childImageSharp.fluid}
-                          />
+                          <StyledImage src={lightTwo.childImageSharp.fluid} />
                         </ImageWrapper>
                         <ImageWrapper>
-                          <StyledImage
-                            loading="eager"
-                            src={lightThree.childImageSharp.fluid}
-                          />
+                          <StyledImage src={lightThree.childImageSharp.fluid} />
                         </ImageWrapper>
                       </ImagesMobile>
 
@@ -217,13 +205,11 @@ function AboutPhotographs() {
                         <ImagesReverse ref={rowTwoRef}>
                           <ImageWrapper hideOnMobile>
                             <StyledImage
-                              loading="eager"
                               src={lightThree.childImageSharp.fluid}
                             />
                           </ImageWrapper>
                           <ImageWrapper>
                             <StyledImage
-                              loading="eager"
                               src={lightFour.childImageSharp.fluid}
                             />
                           </ImageWrapper>
@@ -236,46 +222,28 @@ function AboutPhotographs() {
                   <animated.div style={rowThreeStyles}>
                     <Images ref={rowThreeRef}>
                       <ImageWrapper>
-                        <StyledImage
-                          loading="eager"
-                          src={darkOne.childImageSharp.fluid}
-                        />
+                        <StyledImage src={darkOne.childImageSharp.fluid} />
                       </ImageWrapper>
                       <ImageWrapper hideOnMobile>
-                        <StyledImage
-                          loading="eager"
-                          src={darkTwo.childImageSharp.fluid}
-                        />
+                        <StyledImage src={darkTwo.childImageSharp.fluid} />
                       </ImageWrapper>
                     </Images>
                   </animated.div>
                   <ImagesMobile>
                     <ImageWrapper>
-                      <StyledImage
-                        loading="eager"
-                        src={darkTwo.childImageSharp.fluid}
-                      />
+                      <StyledImage src={darkTwo.childImageSharp.fluid} />
                     </ImageWrapper>
                     <ImageWrapper>
-                      <StyledImage
-                        loading="eager"
-                        src={darkThree.childImageSharp.fluid}
-                      />
+                      <StyledImage src={darkThree.childImageSharp.fluid} />
                     </ImageWrapper>
                   </ImagesMobile>
                   <animated.div style={rowFourStyles}>
                     <ImagesReverse ref={rowFourRef}>
                       <ImageWrapper hideOnMobile>
-                        <StyledImage
-                          loading="eager"
-                          src={darkThree.childImageSharp.fluid}
-                        />
+                        <StyledImage src={darkThree.childImageSharp.fluid} />
                       </ImageWrapper>
                       <ImageWrapper>
-                        <StyledImage
-                          loading="eager"
-                          src={darkFour.childImageSharp.fluid}
-                        />
+                        <StyledImage src={darkFour.childImageSharp.fluid} />
                       </ImageWrapper>
                     </ImagesReverse>
                   </animated.div>
